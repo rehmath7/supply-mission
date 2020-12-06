110950d8d22024f57756fbbe7bfc27d618d3f0d1
rehmath7 <72341136+rehmath7@users.noreply.github.com>
2020-12-06 17:13:39 +0530

    Add files via upload

---
+++ box.js
@@ -0,0 +1,20 @@
+class Box{
+constructor(x,y,width,height){
+var options = {
+    restitution:1
+}
+
+this.body=Bodies.rectangle(x,y,width,height,options);
+World.add(World,this.body)
+}
+
+display(){
+var pos = this.body.position;
+rectMode(CENTER);
+fill (255,0,0);
+rect(pos.x,pos.y,this.width,this.height);
+
+
+
+}
+}
